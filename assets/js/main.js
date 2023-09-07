6a8414968433f923abad734ba498e9e80d7f9ad1
renee-maia <contato@reneemaia.com>
2023-09-07 12:43:58 -0300

    feat: implemented show/hide modal logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,62 +24,70 @@ function convertPokemonToLi(pokemon) {
           <div class="buttonModal">
           <button onclick="openModal(${
             pokemon.number
-          })" class="open-modal">More</button>
+          })" class="modalButton">More</button>
           </div>
 
-<div>____________________</div>
-
-          <div id="modal_${pokemon.number} class="modal">
+          <dialog id="modal_${pokemon.number}" class="modal">
           <button onclick="closeModal(${
             pokemon.number
-          })" class="open-modal">Close</button>
-          <div class="modal-top ${pokemon.type}" >
-  <h2 class="nameModal">${pokemon.name}</h2>
-  <h2 class="numberModal">${pokemon.number}</h2>
-
-  <div class="detail">
-    <ol class="types">
-        ${pokemon.types
-          .map((type) => `<li class="type ${type}">${type}</li>`)
-          .join("")}
-    </ol>
-
-    <img src="${pokemon.photo}"
-         alt="${pokemon.name}">
-</div>
-
-<div class="modal-bottom">
-<h1 class="aboutModal">About</h1>
-<ol class="modalDetailsList">
-  <li class="modalDetailsList">Height: ${pokemon.height}</li>
-  <li class="modalDetailsList">Weight: ${pokemon.weight}</li>
-  <div class="modalDetailsList">
-    <ol class="abilitiesMoves">Abilites: 
-        ${pokemon.abilities
-          .map((ability) => `<li class="ability ${ability}">${ability}</li>`)
-          .join("")}
-    </ol><ol class="abilitiesMoves">Moves: 
-    ${pokemon.moves
-      .map((move) => `<li class="ability ${move}">${move}</li>`)
-      .join("")}
-</ol>
-
-
-</ol>
-</div>
-  </div>
-</div>
+          })" class="modalButton">Close</button>
+          <div>
+            <div class="modal-top ${pokemon.type}">
+              <h2 class="nameModal">${pokemon.name}</h2>
+              <h2 class="numberModal">${pokemon.number}</h2>
+  
+              <div class="detail">
+                <ol class="types">
+                  ${pokemon.types
+                    .map(
+                      (type) => `
+                  <li class="type ${type}">${type}</li>
+                  `
+                    )
+                    .join("")}
+                </ol>
+  
+                <img src="${pokemon.photo}" alt="${pokemon.name}" />
+              </div>
+            </div>
+            <div class="modal-bottom">
+              <h1 class="aboutModal">About</h1>
+              <ol class="modalDetailsList">
+                <li class="modalDetailsList">Height: ${pokemon.height}</li>
+                <li class="modalDetailsList">Weight: ${pokemon.weight}</li>
+              </ol>
+              <ol class="abilitiesMoves">
+                Abilites: ${pokemon.abilities
+                  .map(
+                    (ability) => `
+                <li class="ability ${ability}">${ability}</li>
+                `
+                  )
+                  .join("")}
+              </ol>
+              <ol class="abilitiesMoves">
+                Moves: ${pokemon.moves
+                  .map(
+                    (move) => `
+                <li class="ability ${move}">${move}</li>
+                `
+                  )
+                  .join("")}
+              </ol>
+            </div>
           </div>
-      </li>
+        </dialog>
+
+          </li>
   `;
 }
 
-function openModal(id) {
-  const modal = document.getElementById(`modal_${id}`);
-}
-function closeModal(id) {
-  const modal = document.getElementById(`modal_${id}`);
-}
+// function openModal(id) {
+//   const modal = document.getElementById(`modal_${id}`);
+// }
+// function closeModal(id) {
+//   const modal = document.getElementById(`modal_${id}`);
+// }
 
 function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
@@ -103,3 +111,13 @@ loadMoreButton.addEventListener("click", () => {
     loadPokemonItens(offset, limit);
   }
 });
+
+function openModal(pokemonNumber) {
+  const modal = document.getElementById(`modal_${pokemonNumber}`);
+  modal.showModal();
+}
+
+function closeModal(pokemonNumber) {
+  const modal = document.getElementById(`modal_${pokemonNumber}`);
+  modal.close();
+}
